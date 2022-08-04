fabd85ef1514aea0cd3aa5fac8b923eba88bf613
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-08-03 21:20:52 -0300

    feat: hex working

--- src/parse/flags.c
+++ src/parse/flags.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 21:36:19 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/08/03 13:55:43 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/08/03 21:14:56 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,6 @@ void	space(t_param *spec)
 void	hash(t_param *spec)
 {
 	spec->flags[HASH] = TRUE;
-	spec->prefix = "0x";
 	spec->prefix_size = 2;
 }
 
--- src/print/out_hex.c
+++ src/print/out_hex.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 17:24:56 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/08/02 22:02:22 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/08/03 21:19:29 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,55 @@
 
 // XXX: Flags proibidas: ' ' '+'
 // permitidas: '-' '0' largura precisão
+void	set_unsigned_prefix(t_param *spec);
+void	set_number_precision(t_param *spec, int strlen);
+
 int	out_hex(t_param *spec, va_list args)
 {
 	unsigned int	nbr;
-	char			*u_nbr;
+	char			*string;
 	int				written;
+	int				strlen;
 
+	written = 0;
 	nbr = va_arg(args, unsigned int);
-	if (!nbr)
-		spec->alt = FALSE;
-	if (spec->code == 'X')
-	{
-		spec->hex_prefix = "0X";
-		u_nbr = itoa_base(nbr, UPPER_HEX);
-	}
+	// TODO: mover para uma função
+	if (spec->code == 'x')
+		string = itoa_base(nbr, LOWER_HEX);
 	else
-		u_nbr = itoa_base(nbr, LOWER_HEX);
-	written = dispatcher(u_nbr, spec, ft_strlen(u_nbr));
-	free(u_nbr);
+		string = itoa_base(nbr, UPPER_HEX);
+	strlen = ft_strlen(string);
+
+	if (!nbr && !spec->precision)
+	{
+		string[0] = 0;
+		strlen = 0;
+	}
+	set_number_precision(spec, strlen);
+	if (spec->prefix_size && nbr > 0)
+		set_unsigned_prefix(spec);
+	if (spec->flags[LEFT])
+		written += left_justify(spec, string, strlen);
+	else
+		written += right_justify(spec, string, strlen);
+	free(string);
 	return (written);
+}
+
+void set_number_precision(t_param *spec, int strlen)
+{
+	if (spec->precision < strlen)
+		spec->precision = 0;
+	else
+		spec->precision -= strlen;
+}
+
+void	set_unsigned_prefix(t_param *spec)
+{
+	if (spec->code == 'x')
+		spec->prefix = "0x";
+	else if (spec->code == 'X')
+		spec->prefix = "0X";
+	else
+		spec->prefix = "";
 }
