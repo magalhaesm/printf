546806d7931cf298cddc95588a45e531b41fa022
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-08-03 22:38:34 -0300

    feat: char working

--- src/print/out_char.c
+++ src/print/out_char.c
@@ -6,15 +6,12 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 14:01:31 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/08/02 15:51:55 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/08/03 22:37:11 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/ft_printf.h"
 #include "../../include/printer.h"
-
-// XXX: argumentos proibidos: '+' ' ' '#' '0' precisão
-// permitidas: '-' largura
 
 int	out_char(t_param *spec, va_list args)
 {
@@ -24,10 +21,9 @@ int	out_char(t_param *spec, va_list args)
 	written = 0;
 	chr = va_arg(args, int);
 	spec->width -= 1;
-	if (spec->width)
+	if (!spec->flags[LEFT])
 		written += put_width(spec);
 	written += put_string(&chr, 1);
-	if (spec->flags[LEFT])
-		written += put_width(spec);
+	written += put_width(spec);
 	return (written);
 }
